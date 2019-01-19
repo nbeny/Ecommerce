8d70185eab24a2efc0256073086d230ed97b6937
nbeny <nbeny@student.42.fr>
2019-01-19 06:40:50 +0100

    corrected the protection images path =D

--- src/products/models.py
+++ src/products/models.py
@@ -54,7 +54,7 @@ class Product(models.Model):  # ProductCategory
     title       = models.CharField(max_length=120)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
-    image       = models.ImageField(upload_to='products/', null=True, blank=True)
+    image       = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     featured    = models.BooleanField(default=False)
     active      = models.BooleanField(default=True)
 
