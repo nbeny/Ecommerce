7115a39f7f4043a9091dce021b726695012fec98
nbeny <nbeny@student.42.fr>
2019-01-28 10:40:18 +0100

    bug cart found + adding orders views and templates

--- src/carts/views.py
+++ src/carts/views.py
@@ -18,6 +18,7 @@
 
 from django.shortcuts import render, redirect
 
+from orders.models import Order
 from products.models import Product
 from .models import Cart
 
@@ -25,6 +26,7 @@ from .models import Cart
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
     return render(request, 'carts/home.html', {'cart': cart_obj})
+
 
 def cart_update(request):
     print(request.POST)
@@ -44,3 +46,13 @@ def cart_update(request):
     # cart_obj.products.remove(obj)
     # return redirect(product_obj.get_absolute_url())
     return redirect('cart:home')
+
+
+def checkout_home(request):
+    cart_obj, cart_created = Cart.objects.new_or_get(request)
+    order_obj = None
+    if not cart_created or cart_obj.products.count() == 0:
+        return redirect('cart:home')
+    else:
+        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
+    return render(request, 'carts/checkout.html', {'object': order_obj})
